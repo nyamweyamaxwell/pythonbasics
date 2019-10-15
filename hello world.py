7a2b1a2a83873ed6226453ef3653d91170465372
nyamweyamaxwell <nyamweyamaxwell@gmail.com>
2019-10-15 18:40:07 +0300

    pythin basics

---
+++ hello world.py
@@ -0,0 +1,15 @@
+#helloworld
+#print 'hello world' to the console
+print('Hello')
+name = input('what is your name')
+print('Nice to meet you ' + name)
+#creating a variable. The sum is stored in the variable
+total = 30+30+60
+print(total)
+#white spaces are ignored
+total = 30+   30   +   60
+print(total)
+#literal - string literal
+print('total')
+'''another comment shortcut'''
+"""yet another comment shortcut"""
---
+++ variables.py
@@ -0,0 +1,4 @@
+first_num = 55
+second_num = 68
+total = int(first_num)+int(second_num)
+print(total)
